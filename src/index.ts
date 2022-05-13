086ecc403c1ff3928d22741bde484c1ce072df54
Jordan Katz <jordankatz@jordans-mbp.westport.k12.ct.us>
2022-05-13 14:37:08 -0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import fs from 'fs';
+import path from 'path';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import Discord from 'discord.js';
+import std from '@jordanbkatz/std';
+
+console.log(std.rand.int(5, 10));
+
+// dotenv.config();
+// const intents = new Discord.Intents(32767);
+// const bot = new Discord.Client({ intents });
+
+// console.log(process.env.ASDF);
