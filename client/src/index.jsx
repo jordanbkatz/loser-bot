e8cb30dce96f94b9db27145f1d3e93f3f662a3db
jordanbkatz <jordanbkatz4@gmail.com>
2022-08-26 17:05:23 -0400

    removed paypal provider from client

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import App from './App';
 import './styles/main.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <PayPalScriptProvider options={{ "client-id": process.env.REACT_APP_PAYPAL_ID }}>
-            <App />
-        </PayPalScriptProvider>
+        <App />
     </React.StrictMode>
 );
